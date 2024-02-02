b37fcc13882691ff62274252507a707ca2d6e3ac
Tomas Lugo <lugoleandro@hotmail.com>
2024-02-02 02:42:38 -0300

    responsive

--- src/components/AccordionCard/AccordionCard.tsx
+++ src/components/AccordionCard/AccordionCard.tsx
@@ -19,13 +19,13 @@ const AccordionCard = ({
   repo,
 }: AccordionCardProps) => {
   return (
-    <div className="grid grid-cols-2 grid-rows-1 gap-4 px-2">
+    <div className="grid grid-cols-1 md:grid-cols-2 md:grid-rows-1 gap-4 px-2">
       {url ? (
         <a
           href={url}
           target="_blank"
           rel="noreferrer"
-          className="row-end-1 hover:opacity-80 transition-all border border-transparent hover:border-white relative cursor-none"
+          className="md:row-end-1 hover:opacity-80 transition-all border border-transparent hover:border-white relative cursor-none"
           data-interactive
         >
           <div className="absolute inset-0 w-full h-full bg-black/50 opacity-0 hover:opacity-100  flex flex-col gap-0 items-center justify-center transition-all">
@@ -45,7 +45,7 @@ const AccordionCard = ({
           className="aspect-video object-center object-cover row-end-1"
         />
       )}
-      <div className="flex flex-col gap-2 row-end-1">
+      <div className="flex flex-col gap-2 md:row-end-1">
         <p className="h-full">{description}</p>
         <div className="flex flex-wrap gap-2">
           {stack.map((tech, i) => (
@@ -60,7 +60,7 @@ const AccordionCard = ({
         </div>
       </div>
       {repo ? (
-        <div className="row-start-1 row-end-2 w-fit" data-interactive>
+        <div className="md:row-start-1 md:row-end-2 w-fit" data-interactive>
           <Badge
             className="rounded text-xs p-0 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground text-white px-3 py-1"
             variant="default"
---
+++ todo.md
@@ -0,0 +1,7 @@
+# Todos
+
+TODO => add cv
+TODO => add jobs
+TODO => add readme
+TODO => add tests
+TODO => **_OPT_** add about me image
