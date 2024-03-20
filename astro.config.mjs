e268f106d7d9d5c0b877f3cb9b04e58c1f11c7f8
Tomas Lugo <lugoleandro@hotmail.com>
2024-03-20 18:26:14 -0300

    robots and sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from 'astro-robots-txt';
+
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lugo-tomas-portfolio.vercel.app',
+
   integrations: [react(), tailwind({
-    applyBaseStyles: false,
-  }),]
+    applyBaseStyles: false
+  }), sitemap(), robotsTxt()]
 });
